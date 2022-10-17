e525ce5081bb96110f9ec9b90a290b87c21ba0c3
Ra1m1380 <114662256+Ra1m1380@users.noreply.github.com>
2022-10-17 23:23:56 +0330

    main page light

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily:{
+      vazir:['vazir']
+    },
+    extend: {
+      screens: {
+        'sm': '459px',
+      },
+    },
   },
   plugins: [],
 }
